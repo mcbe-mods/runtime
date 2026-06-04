e3091a258095056f9f4f7cbc077a1a2505b0b95a
lete114 <me@imlete.cn>
2026-06-04 21:31:48 +0800

    chore: remove unused utils mock from runtime test setup

--- packages/runtime/test/setup.ts
+++ packages/runtime/test/setup.ts
@@ -16,8 +16,3 @@ vi.mock('@minecraft/server', () => ({
     Server: 'Server',
   },
 }))
-
-vi.mock('@mcbe-mods/utils', () => ({
-  ms2ticks: vi.fn((ms: number) => Math.ceil(ms / 50)),
-  calcGameTicks: vi.fn((ms: number) => Math.ceil(ms / 50)),
-}))
