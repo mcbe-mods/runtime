e3dea144571cf1bde0ce56b414cc976557961c89
lete114 <me@imlete.cn>
2026-06-14 10:08:27 +0800

    fix(crypto): replace PBKDF2 with HKDF, use Math.random() for nonce to support MCBE

--- packages/crypto/src/cipher.ts
+++ packages/crypto/src/cipher.ts
@@ -1,25 +1,32 @@
+import type { TRet } from '@noble/ciphers/utils.js'
 import { Base64, utf8Decode, utf8Encode } from '@mcbe-mods/utils'
 import { xchacha20poly1305 } from '@noble/ciphers/chacha.js'
 import { managedNonce } from '@noble/ciphers/utils.js'
-import { pbkdf2 } from '@noble/hashes/pbkdf2.js'
+import { hkdf } from '@noble/hashes/hkdf.js'
 import { sha256 } from '@noble/hashes/sha2.js'
 
 const KEY_LENGTH = 32
-const ITERATIONS = 100_000
+
+function mcRandomBytes(bytesLength = 32): TRet<Uint8Array> {
+  const out = new Uint8Array(bytesLength)
+  for (let i = 0; i < bytesLength; i++)
+    out[i] = (Math.random() * 256) | 0
+  return out as TRet<Uint8Array>
+}
 
 export class Cipher {
   readonly #impl: { encrypt: (data: Uint8Array) => Uint8Array, decrypt: (data: Uint8Array) => Uint8Array }
 
   private constructor(key: Uint8Array) {
-    this.#impl = managedNonce(xchacha20poly1305)(key)
+    this.#impl = managedNonce(xchacha20poly1305, mcRandomBytes)(key)
   }
 
   static fromPassword(password: string, salt?: string | Uint8Array): Cipher {
-    const passwordBytes = utf8Encode(password)
+    const ikm = utf8Encode(password)
     const saltBytes = salt !== undefined
       ? (typeof salt === 'string' ? utf8Encode(salt) : salt)
       : utf8Encode('github.com/mcbe-mods/runtime')
-    const key = pbkdf2(sha256, passwordBytes, saltBytes, { c: ITERATIONS, dkLen: KEY_LENGTH })
+    const key = hkdf(sha256, ikm, saltBytes, undefined, KEY_LENGTH)
     return new Cipher(key)
   }
 
